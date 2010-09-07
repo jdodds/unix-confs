0dd8ecb1170abd96db403ff1c32cdc4af05f711a
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2010-09-07 00:40:08 -0400

    add some default layout and float behaviour

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,16 +4,24 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 import XMonad.Util.Run
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO    
 
 main=do
   conky <- spawnPipe myConkyBar
---  statusBar <- spawnPipe myStatusBar           
+  --  statusBar <- spawnPipe myStatusBar           
   xmonad =<< dzen defaultConfig
     { terminal = "urxvtc"
     , modMask  = mod4Mask
     , borderWidth = 3
     , layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig 
     , manageHook = composeAll [isFullscreen --> doFullFloat
+                              ,className =? "Qjackctl" --> doFloat
+                              ,className =? "Qjackctl" --> doShift "9"
+                              ,className =? "Emacs" --> doShift "2"
+                              ,className =? "Pidgin" --> doFloat
+                              ,className =? "Skype" --> doFloat
+                              ,className =? "Chromium" --> doShift "1"
                               ,manageDocks
                               ]
     --    , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn statusBar }
@@ -27,6 +35,7 @@ myDzenOpts = "-fg '" ++ myBarFgColor ++ "' -bg '" ++ myBarBgColor ++ "' -h '16'"
 myStatusBar = "dzen2 -w 655 -ta l " ++ myDzenOpts
                 
 myConkyBar = "conky | dzen2 -e 'onstart=lower' -w '690' -x '750' -ta r " ++ myDzenOpts
+
 
 
 
